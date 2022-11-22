4186cdaf4087a247b9f40354af9330ec3f150ef0
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-22 15:46:47 +0100

    strutturata base esercizio

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string titolo = "Clean code. Guida per diventare bravi artigiani nello sviluppo agile di software";
+string autore = "Robert C. Martin";
+string isbn = "8850334389";
+int numeroPagine = 435;
+int pesoLibro = 860;
+float larghezza = 17.3f;
+float lunghezza = 2.6f;
+float profondita = 17.2f;
+int numeroRecensioni = 119;
+float valutazioneMedia = 4.5f;
+bool isKindle = true;
+bool isFlessible = true;
+
+
+
+Console.WriteLine("IL LIBRO DI OGGI: " + titolo + " di " + autore);
+
+Console.WriteLine("Informazioni generiche: ");
+Console.WriteLine("ISBN: " + isbn);
+Console.WriteLine("Numero delle pagine: " + numeroPagine + " pagine");
+Console.WriteLine("Peso del libro: " + pesoLibro + " g");
+Console.WriteLine("Dimensione del libro: " + larghezza + " cm X " + lunghezza + " cm X " + profondita + " cm");
+
+Console.WriteLine("Informazione Amazon:");
+Console.WriteLine("Numero di recensioni: " + numeroRecensioni + " recensioni");
+Console.WriteLine("Valutazione media: " + valutazioneMedia + " stelline");
+Console.WriteLine("Kindle disponibile: " + isKindle);
+Console.WriteLine("Copertina flessibile disponibile: " + isFlessible);
