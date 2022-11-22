6dbc1c5d486aaf5398ce4e1a35da3371599f6c02
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-22 16:19:14 +0100

    completato bonus

--- Bonus/Program.cs
+++ Bonus/Program.cs
@@ -1,52 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Titolo libro:");
+string titolo = Console.ReadLine();
 
-string titolo = "Clean code. Guida per diventare bravi artigiani nello sviluppo agile di software";
-string autore = "Robert C. Martin";
-string isbn = "8850334389";
-int numeroPagine = 435;
-int pesoLibro = 860;
-float larghezza = 17.3f;
-float lunghezza = 2.6f;
-float profondita = 17.2f;
-int numeroRecensioni = 119;
-float valutazioneMedia = 4.5f;
-bool isKindle = true;
-bool isFlessible = true;
-string kindle = "";
-string flessible = "";
+Console.WriteLine("Autore:");
+string autore = Console.ReadLine();
 
+Console.WriteLine("ISBN:");
+string isbn = Console.ReadLine();
 
-if (isKindle)
-{
-    kindle = "Sì";
-}
-else
-{
-    kindle = "No";
-}
+Console.WriteLine("Numero pagine:");
+int numeroPagine = int.Parse(Console.ReadLine());
 
-if (isFlessible)
-{
-    flessible = "Sì";
-}
-else
-{
-    flessible = "No";
-}
+Console.WriteLine("Peso Libro:");
+float pesoLibro = float.Parse(Console.ReadLine());
+
+Console.WriteLine("larghezza:");
+float larghezza = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Lunghezza:");
+float lunghezza = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Profondità:");
+float profondita = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Numero recensioni:");
+int numeroRecensioni = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Valutazione media:");
+float valutazioneMedia = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Kindle?");
+string isKindle = Console.ReadLine();
+
+Console.WriteLine("Flessibile?");
+string isFlessible = Console.ReadLine();
+
+Console.WriteLine();
 
 
 Console.WriteLine("IL LIBRO DI OGGI: " + titolo + " di " + autore);
 
+Console.WriteLine();
+
 Console.WriteLine("Informazioni generiche: ");
+
+Console.WriteLine();
+
 Console.WriteLine("ISBN: " + isbn);
 Console.WriteLine("Numero delle pagine: " + numeroPagine + " pagine");
-Console.WriteLine("Peso del libro: " + pesoLibro + " g");
+Console.WriteLine("Peso del libro: " + pesoLibro + " Kg");
 Console.WriteLine("Dimensione del libro: " + larghezza + " cm X " + lunghezza + " cm X " + profondita + " cm");
 
+Console.WriteLine();
+
 Console.WriteLine("Informazione Amazon:");
+
+Console.WriteLine();
+
 Console.WriteLine("Numero di recensioni: " + numeroRecensioni + " recensioni");
 Console.WriteLine("Valutazione media: " + valutazioneMedia + " stelline");
-Console.WriteLine("Kindle disponibile: " + kindle);
-Console.WriteLine("Copertina flessibile disponibile: " + flessible);
+Console.WriteLine("Kindle disponibile: " + isKindle);
+Console.WriteLine("Copertina flessibile disponibile: " + isFlessible); 
+
 
